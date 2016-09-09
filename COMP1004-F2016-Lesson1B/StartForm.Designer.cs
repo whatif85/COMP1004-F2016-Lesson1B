e9f9de09a1fc5d19bf05e1bc2cd088b2960de9f2
whatif85 <geeckoguy@gmail.com>
2016-09-09 08:59:51 -0400

    added label and click me button

---
+++ COMP1004-F2016-Lesson1B/StartForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace COMP1004_F2016_Lesson1B
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MessageLabel = new System.Windows.Forms.Label();
+            this.ClickMeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MessageLabel
+            // 
+            this.MessageLabel.AutoSize = true;
+            this.MessageLabel.Location = new System.Drawing.Point(195, 71);
+            this.MessageLabel.Name = "MessageLabel";
+            this.MessageLabel.Size = new System.Drawing.Size(46, 17);
+            this.MessageLabel.TabIndex = 0;
+            this.MessageLabel.Text = "label1";
+            // 
+            // ClickMeButton
+            // 
+            this.ClickMeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ClickMeButton.Location = new System.Drawing.Point(145, 254);
+            this.ClickMeButton.Name = "ClickMeButton";
+            this.ClickMeButton.Size = new System.Drawing.Size(118, 56);
+            this.ClickMeButton.TabIndex = 1;
+            this.ClickMeButton.Text = "Click Me!";
+            this.ClickMeButton.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 358);
+            this.Controls.Add(this.ClickMeButton);
+            this.Controls.Add(this.MessageLabel);
+            this.Name = "StartForm";
+            this.Text = "Start Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MessageLabel;
+        private System.Windows.Forms.Button ClickMeButton;
+    }
+}
+
